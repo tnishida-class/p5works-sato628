13759fa5fdd6c744c74a57220f0a1959f473cbbc
sato628 <72775405+sato628@users.noreply.github.com>
2020-10-13 13:15:58 +0900

    Update_section1-1_1013

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -5,12 +5,16 @@ function setup() {
 function draw() {
   background(0);
   strokeWeight(4);
-  fill(178, 100, 162);
-  triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
+  fill(199, 247, 136);
+  triangle(12,118,116,118,116,10);
+  fill(232, 111, 146);
   triangle(0,108,106,108,106,0);
   fill(255);
-  textSize(32);
+  textSize(25);
   textFont("serif");
-  text("46", 68, 100);
+  text("46+1", 53, 85);
+  fill(79, 79, 79);
+  textSize(18);
+  textFont("serif");
+  text("sato", 75, 100);
 }
